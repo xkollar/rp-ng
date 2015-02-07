8de4cdd12da898be97dd47a7009b70dfcc9d9819
Matej Kollar <mkollar@redhat.com>
2015-02-07 19:07:29 +0100

    Pythonfix...

--- pythonfix.py
+++ pythonfix.py
@@ -7,12 +7,20 @@ import sys
 
 __all__ = []
 
+
 def export(fun):
     __all__.append(fun.__name__)
     return fun
 
 # pylint: disable=I0011,W0622
 if sys.version_info < (2, 5):
+    @export
+    def all(iterable):
+        for item in iterable:
+            if not item:
+                return False
+        return True
+
     @export
     def any(iterable):
         for item in iterable:
