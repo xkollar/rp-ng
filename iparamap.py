affc9b6e8286c147e1f2c3e49f25d1f8b5cd261c
Matej Kollar <mkollar@redhat.com>
2015-02-10 23:08:09 +0100

    Exception handling needed on even higher level

--- iparamap.py
+++ iparamap.py
@@ -10,13 +10,12 @@ NO_MORE_TASKS = object()
 
 def iparamap(fun, iterator, queue_size=3):
     def worker(queue):
-        for item in iterator:
-            try:
+        try:
+            for item in iterator:
                 value = fun(item)
-            except Exception, e:
-                queue.put(e)
-                break
-            queue.put(value)
+                queue.put(value)
+        except Exception, e:
+            queue.put(e)
         queue.put(NO_MORE_TASKS)
 
     queue = Queue.Queue(queue_size)
@@ -36,13 +35,20 @@ if __name__ == '__main__':
     import time
 
     def fun1(x):
-        raise Exception("WHOA!")
-        print 'Map: Processing %s' % x
         time.sleep(1)
         return x + 100
 
+    def fun2(x):
+        raise Exception("WHOA!")
+        time.sleep(1)
+        return x * 2
+
+    def my_gen():
+        raise Exception("Whoa gen!")
+        yield 0
+
     try:
-        for item in iparamap(fun1, xrange(10)):
+        for item in iparamap(fun1, iparamap(fun1, xrange(10))):
             print 'Main: Processing %s...' % item
             time.sleep(5)
     except Exception, e:
