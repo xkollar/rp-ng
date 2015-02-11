2d1c1c8fc8423075ec0978ada5c8a2a2c3298d53
Matej Kollar <mkollar@redhat.com>
2015-02-11 14:21:03 +0100

    Cleanup

--- iparamap.py
+++ iparamap.py
@@ -12,8 +12,7 @@ def iparamap(fun, iterator, queue_size=3):
     def worker(queue):
         try:
             for in_item in iterator:
-                value = fun(in_item)
-                queue.put(value)
+                queue.put(fun(in_item))
         except Exception, exn:  # pylint: disable=W0703
             queue.put(exn)
         queue.put(NO_MORE_TASKS)
