846c8488d3a8675a1f3512407ab171556e016dfc
Matej Kollar <mkollar@redhat.com>
2015-02-11 14:09:22 +0100

    Cleanup

--- iparamap.py
+++ iparamap.py
@@ -29,26 +29,3 @@ def iparamap(fun, iterator, queue_size=3):
         yield ret_item
         ret_item = queue.get()
     thread.join()
-
-# TEST
-
-if __name__ == '__main__':
-    import time
-
-    def fun1(val):
-        time.sleep(1)
-        return val + 100
-
-    def fun2(_val):
-        time.sleep(1)
-        raise Exception("WHOA!")
-
-    def my_gen():
-        raise Exception("Whoa gen!")
-
-    try:
-        for x_item in iparamap(fun1, iparamap(fun1, xrange(10))):
-            print 'Main: Processing %s...' % x_item
-            time.sleep(5)
-    except Exception, exn:  # pylint: disable=W0703
-        print 'PFS!', exn.message
